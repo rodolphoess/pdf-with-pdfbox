96bdaa7144576649fea68c8319e9dcd5dfd25255
Rodolpho Sousa <rodolpho.sousa@bancointer.com.br>
2020-07-06 10:26:13 -0300

    Importação e leitura de PDF

--- src/br/pdfbox/Main.java
+++ src/br/pdfbox/Main.java
@@ -1,9 +1,36 @@
 package br.pdfbox;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-        //Teste
+
+        try {
+
+            Long tempoInicial = System.currentTimeMillis();
+            File file = new File("../pdf-with-pdfbox/src/br/pdfbox/pdf-fundos/extrato-completo.pdf");
+
+            PDDocument document = PDDocument.load(file);
+
+            PDFTextStripper pdfTextStripper = new PDFTextStripper();
+
+            String conteudo = pdfTextStripper.getText(document);
+            Long tempoFinal = System.currentTimeMillis();
+
+            System.out.println(conteudo);
+
+            Long tempoExecucao = tempoFinal - tempoInicial;
+
+            System.out.println("--------------------------------\n\n" + "Tempo de execução: " + tempoExecucao + "ms");
+
+        } catch (Exception e) {
+            System.out.print(e);
+        }
+
     }
 
 }
