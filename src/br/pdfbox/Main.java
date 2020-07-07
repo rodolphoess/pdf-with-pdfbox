bb7e6ac0e912c516822ed5e463f547a52f75957a
Rodolpho Sousa <rodolpho.sousa@bancointer.com.br>
2020-07-07 16:18:37 -0300

    Separação da String completa em uma lista de Strings aonde cada item representa um cliente

--- src/br/pdfbox/Main.java
+++ src/br/pdfbox/Main.java
@@ -4,6 +4,11 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 public class Main {
 
@@ -19,20 +24,49 @@ public class Main {
             PDDocument document = PDDocument.load(file);
 
             PDFTextStripper pdfTextStripper = new PDFTextStripper();
+            pdfTextStripper.setWordSeparator(" | ");
+            pdfTextStripper.setLineSeparator(" || ");
 
             String conteudo = pdfTextStripper.getText(document);
             long tempoFinal = System.currentTimeMillis();
 
-            System.out.println(conteudo);
+            List<StringBuilder> clientes = quebrarStringPorCliente(conteudo);
+
+            for (StringBuilder cliente : clientes) {
+                System.out.println(cliente);
+            }
 
             long tempoExecucao = tempoFinal - tempoInicial;
 
             System.out.println("--------------------------------\n\n" + "Tempo de execução: " + tempoExecucao + "ms");
 
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.print(e);
         }
 
+    }
+
+    private static List<StringBuilder> quebrarStringPorCliente(String conteudo) {
+
+        List<StringBuilder> clientes = newArrayList();
+        Scanner scanner = new Scanner(conteudo);
+        StringBuilder cliente = new StringBuilder();
+
+        while (scanner.hasNextLine()) {
+
+            String linha = scanner.nextLine();
+            cliente.append(linha);
+
+            if (linha.equals("")) {
+                clientes.add(cliente);
+
+                cliente = new StringBuilder();
+            }
+        }
+
+        scanner.close();
+
+        return clientes;
     }
 
 }
